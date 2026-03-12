7f7194cc61f5879892f261abfc769cdf481c76a0
Luke Brittain <luke.brittain@gmail.com>
2026-03-12 10:04:03 -0500

    done

--- Main.java
+++ Main.java
@@ -4,9 +4,20 @@ public class Main{
         int target = 7;
         int result = binarySearch(sortedArray, target, 0, sortedArray.length - 1);
         System.out.println("Target found at index: " + result);
-    }
+    }    public static int binarySearch(int[] array, int target, int left, int right) {
+        // Base case: search space is empty
+        if (left > right) {
+            return -1;
+        }
 
-    public static int binarySearch(int[] array, int target, int left, int right) {
-        
+        int mid = (left + right) / 2;
+
+        if (array[mid] == target) {
+            return mid; // Target found
+        } else if (target < array[mid]) {
+            return binarySearch(array, target, left, mid - 1); // Search left half
+        } else {
+            return binarySearch(array, target, mid + 1, right); // Search right half
+        }
     }
 }
